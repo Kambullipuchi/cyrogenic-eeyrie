23ebe0dee866169cd5eecd623eadbfa64d4bdff4
Data Chand <data.chand@gmail.com>
2015-09-13 22:02:06 +0530

    Heroku API Translator Service

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,8 @@ class WelcomeController < ApplicationController
 	def index
 		heroku = PlatformAPI.connect_oauth('6b8979c5-ee85-4e5b-8f61-75c50e3ecac2')
 		@details = heroku.app.info('cyrogenic-eeyrie')
-		render :json => @details
+		@list = heroku.app.list()
+		@dynos = heroku.dyno.list('cyrogenic-eeyrie')
+		# render :json => @dynos
 	end
 end
