990e65ddd8a742810e2223802368507ef55d959e
Data Chand <data.chand@gmail.com>
2015-09-13 16:38:33 +0530

    Platform API

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
+require 'platform-api'
+
 class WelcomeController < ApplicationController
+	def index
+		heroku = PlatformAPI.connect_oauth('6b8979c5-ee85-4e5b-8f61-75c50e3ecac2')
+		@details = heroku.app.info('cyrogenic-eeyrie')
+		render :json => @details
+	end
 end
